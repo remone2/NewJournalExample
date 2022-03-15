b3d735846b666b244bc2794c06b924a590c256f6
remone2 <jamodinm@yahoo.com>
2022-03-15 10:35:19 -0500

    Set up practise

--- NewJournalExample/Models/Journal.cs
+++ NewJournalExample/Models/Journal.cs
@@ -7,6 +7,9 @@ namespace NewJournalExample.Models
         [Key]
         [Display(Name = "Journal Entry #")]
         public int JournalNumber { get; set; }
-
+        [StringLength(50, MinimumLength = 5)]
+        public string Title { get; set; }
+        [StringLength(100, MinimumLength = 10)]
+        public string Content { get; set; }
     }
 }
--- NewJournalExample/Models/User.cs
+++ NewJournalExample/Models/User.cs
@@ -7,6 +7,14 @@ namespace NewJournalExample.Models
         [Key]
         [Display(Name = "User Number")]
         public int UserNumber { get; set; }
-
+        [StringLength(50, MinimumLength = 6)]
+        public string UserName { get; set; }
+        public ICollection<Journal> Journals { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public User()
+        {
+            Journals = new HashSet<Journal>();
+            Comments = new HashSet<Comment>();
+        }
     }
 }
